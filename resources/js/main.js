f0c284f20b0cdb4f4a906b5fd003536d40e029f8
Rasel Rana Earshad <charry.br10@gmail.com>
2023-09-10 14:50:33 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+// Testimonial Slider
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+});
+// Mobile Menu
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
